b05c788b6a55c0085b9e3bbde1cee9826f63510a
Vincent ferrante <vincentferrante@mac.mynetworksettings.com>
2026-01-27 18:10:31 -0500

    Improve Est 2026 badge aesthetics and centering

--- components/About.tsx
+++ components/About.tsx
@@ -17,7 +17,7 @@ export const About: React.FC = () => {
                   className="w-full h-full object-cover grayscale opacity-80"
                 />
               </div>
-              <div className="absolute -bottom-10 -right-10 bg-indigo-600 text-white p-10 rounded-[30px] shadow-2xl hidden md:block">
+              <div className="absolute -bottom-8 left-1/2 -translate-x-1/2 bg-gradient-to-r from-indigo-600 to-violet-600 text-white py-6 px-12 rounded-[30px] shadow-2xl shadow-indigo-500/30 border border-white/20 hidden md:block whitespace-nowrap">
                 <span className="text-4xl font-black italic tracking-tighter">Est. 2026</span>
               </div>
             </div>
